628eb6a6e6d92ca5eb0132c17fcf156f24319f9b
phuchau23 <phuchaulua123@gmail.com>
2026-03-11 19:12:23 +0700

    Handle Analysis for Web

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Providers from "@/lib/providers";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toast";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin", "vietnamese"],
@@ -9,16 +10,13 @@ const inter = Inter({
   display: "swap",
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="vi" className={inter.variable} suppressHydrationWarning>
       <body className={`min-h-screen font-sans antialiased ${inter.className}`}>
         <Toaster position="bottom-center" />
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
